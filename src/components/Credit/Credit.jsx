2d716bfee99d33d09d4bda187447180b7e71bc75
Zheng Qiao <109362533+BossJoeZz@users.noreply.github.com>
2024-03-25 11:46:34 -0700

    Update Credit.jsx

--- src/components/Credit/Credit.jsx
+++ src/components/Credit/Credit.jsx
@@ -6,7 +6,7 @@ export default function Credit() {
             <h2 className="credit">
                 Here is My github Repo:
             </h2>
-            <a href="www.google.com" className="myLink">Conway's Game of Live</a>
+            <a href="https://github.com/BossJoeZz/Zheng-Qiao-assignment2/tree/main" className="myLink">Conway's Game of Live</a>
             <div className="credit">
                 Welcome and enjoy it
             </div>
